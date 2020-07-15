e321dbb94a25de938debcb15141d22d8f340e762
Luis Yauri <david.yauri147@gmail.com>
2020-07-15 10:26:19 -0500

    Estructura básica

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+//void main() {
+//  runApp(MaterialApp(
+//    home: Text('Hola Equipo'),
+//  ));
+//}
+
+void main() => runApp(MaterialApp(
+      home: Text('Hola Mundo :)'),
+    ));
