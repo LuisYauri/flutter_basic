fdffbaa58e7fe62946018e0e823903407624c131
Luis Yauri <david.yauri147@gmail.com>
2020-07-17 10:25:33 -0500

    Agregar Scaffold con appbar, body y floatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -7,5 +7,17 @@ import 'package:flutter/material.dart';
 //}
 
 void main() => runApp(MaterialApp(
-      home: Text('Hola Mundo :)'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Chino Mandarín Group'),
+          centerTitle: true,
+          elevation: 10,
+          backgroundColor: Colors.red,
+        ),
+        body: Text('Emprendimiento :)'),
+        floatingActionButton: FloatingActionButton(
+          child: Text('+'),
+          onPressed: null,
+        ),
+      ),
     ));
